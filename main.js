0a139d0b30253141872f5e3097a05a6f853ed0ea
Grégory Van Ossel <63047472+GregVo23@users.noreply.github.com>
2021-05-02 11:06:33 +0200

    lesson 3

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 const product = 'Socks'
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Chaussettes', // updated data value //
+            description: 'De bien belle chaussettes, disponible en deux couleur: verte ou bleue',
+            image: './assets/images/socks_green.jpg',
+            image_blue: './assets/images/socks_blue.jpg',
+            url: 'https://www.vanossel.be',
+            onsale: true,
+            stock: 9,
+        }
+    }
+})
