bab52560c72bda836cc9b1ffc6446b3eff4cdc2b
Grégory Van Ossel <vanosselgregory@gmail.com>
2021-05-02 15:18:50 +0200

    lesson 6

--- main.js
+++ main.js
@@ -4,13 +4,31 @@ const product = 'Socks'
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Chaussettes', // updated data value //
+
+            cart: 0,
+            product: 'Chaussettes',
             description: 'De bien belle chaussettes, disponible en deux couleur: verte ou bleue',
             image: './assets/images/socks_green.jpg',
-            image_blue: './assets/images/socks_blue.jpg',
             url: 'https://www.vanossel.be',
             onsale: true,
             stock: 9,
+            details: ['XXl, ' ,'Xl, ' ,'L, ' ,'M, ' ,'S, ',],
+            products: [
+                { id: 123, color: 'green', price: 19, image: './assets/images/socks_green.jpg' },
+                { id: 241, color: 'blue', price: 19, image: './assets/images/socks_blue.jpg' },
+            ],
+
+
+
         }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+            console.log('ok')
+        },
+        updateImage(productImage){
+            this.image = productImage
+        },
     }
 })
